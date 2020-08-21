96d8d0d1d46d0d965f39c8cb99d4e078c113c8e3
Kaymind <lempelziv@gmail.com>
2020-08-21 23:20:57 +0700

    Updates

---
+++ precache-manifest.747013c79c969e9747a7c67438501df1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cef8a21d4f8abdd10f6e18891deb7fc",
+    "url": "/appman-test/index.html"
+  },
+  {
+    "revision": "054c551c10053c8e14ab",
+    "url": "/appman-test/static/css/main.879ea0de.chunk.css"
+  },
+  {
+    "revision": "e983b73e271f4ebd8ece",
+    "url": "/appman-test/static/js/2.7da89604.chunk.js"
+  },
+  {
+    "revision": "a38179c3a123c4a544eacdbb77f01e24",
+    "url": "/appman-test/static/js/2.7da89604.chunk.js.LICENSE"
+  },
+  {
+    "revision": "054c551c10053c8e14ab",
+    "url": "/appman-test/static/js/main.1d55a716.chunk.js"
+  },
+  {
+    "revision": "11b602b8bebedca13a0a",
+    "url": "/appman-test/static/js/runtime-main.80a745a0.js"
+  },
+  {
+    "revision": "c5dd531af1ba9d4e54efa33581228b13",
+    "url": "/appman-test/static/media/cute.c5dd531a.png"
+  },
+  {
+    "revision": "42300f5b629edf7877a3e9c53db37c0a",
+    "url": "/appman-test/static/media/search.42300f5b.png"
+  }
+]);
